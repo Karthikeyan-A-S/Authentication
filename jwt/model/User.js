e2a01df478445189105df51e4633d0a9d62882ad
Karthikeyan-A-S <karthikeyanas.official@gmail.com>
2026-01-28 14:27:51 +0530

    session authentication

--- jwt/model/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = mongoose.Schema(
-    {
-        username:String,
-        password:String
-    }
-);
-exports.User = mongoose.model("User",UserSchema);
